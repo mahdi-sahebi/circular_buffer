9c6e3ab6cb5193f31fa435d3051fa7977f5fadf3
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2024-07-09 14:18:04 +0330

    [TEST]: Common tests are implemented

--- test/test_circular_buffer.cpp
+++ test/test_circular_buffer.cpp
@@ -39,22 +39,65 @@ TEST(get_free, valid)
 
 TEST(write, overflow)
 {
-  CircularBuffer buffer(10);
+  constexpr uint32_t SIZE = 10;
+  CircularBuffer buffer(SIZE);
+  EXPECT_EQ(SIZE, buffer.GetCapacity());
+
   vector<char> data(20, 'a');
 
   EXPECT_THROW({
-    buffer.write(data, data.size());
+    buffer.Write(data, data.size());
   }, overflow_error);
+
+  EXPECT_EQ(SIZE, buffer.GetFreeSize());
 }
 
 TEST(write, valid)
 {
-  CircularBuffer buffer(10);
+  constexpr uint32_t SIZE = 10;
+  CircularBuffer buffer(SIZE);
+
   vector<char> data(7, 'a');
 
   EXPECT_NO_THROW({
+    buffer.Write(data, data.size());
+  });
+
+  EXPECT_EQ(SIZE - data.size(), buffer.GetFreeSize());
+}
+
+
+TEST(read, overflow)
+{
+  constexpr uint32_t SIZE = 100;
+  CircularBuffer buffer(SIZE);
+
+  vector<char> data(20, 'a');
+  buffer.Write(data, data.size());
+  EXPECT_EQ(SIZE - data.size(), buffer.GetFreeSize());
+
+  EXPECT_THROW({
+    vector<char> data = buffer.Read(87);
+  }, out_of_range);
+
+  EXPECT_EQ(SIZE - data.size(), buffer.GetFreeSize());
+}
+
+TEST(read, valid)
+{
+  CircularBuffer buffer(10);
+  vector<char> data(17, 'a');
+
+  EXPECT_NO_THROW({
     buffer.write(data, data.size());
-  }, overflow_error);
+  });
+  EXPECT_EQ(SIZE - data.size(), buffer.GetFreeSize());
+
+  EXPECT_NO_THROW({
+    vector<char> data = buffer.Read(8);
+  });
+
+  EXPECT_EQ(SIZE - data.size() + 8, buffer.GetFreeSize());
 }
 
 int main()
